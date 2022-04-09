039df9d1bba6838b695e74679263bbf51c9a300b
gabiqb <tus_gabi@yahoo.com>
2022-04-09 03:02:46 +0300

    update

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@
    networks: {
      ropsten:{ 
        url: 'https://eth-ropsten.alchemyapi.io/v2/y5luOmFd8TmItHkmHu6fGEchTfvRy03e',
-       accounts: ['cf140bb7ee355fd8c901610d628030124b80b7ed8210ae5fe951bb2c4e6bdd03']
+       accounts: ['account private key']
      }
    }
  }
